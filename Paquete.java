40cf2b4ed377e1ab09a0c1a526a0912090964fd2
Endika Ciordia <eciordiafdam1@gmail.com>
2021-11-03 20:16:04 +0100

    Constructor 1 crear el paquete con los valores,30,40 y 50.

--- Paquete.java
+++ Paquete.java
@@ -18,109 +18,112 @@ public class Paquete
      * alto, ancho, largo (en cm) y peso 8 Kg
      */
     public Paquete()    {
-        //TODO
+        Alto = 40cm;
+        Ancho = 30cm;
+        Largo = 50cm;
+        Peso = 8kg;
          
 
     }
 
-    /**
-     * Constructor 2 
-     *  Crea el paquete con el alto, ancho, largo (en cm) indicados
-     *  y peso un valor aleatorio entre 2 y 8 Kg inclusive
-     *  Se crea aquí el generador
-     */
-    public Paquete(double alto, double ancho, double largo)    {
-        //TODO
+    // /**
+     // * Constructor 2 
+     // *  Crea el paquete con el alto, ancho, largo (en cm) indicados
+     // *  y peso un valor aleatorio entre 2 y 8 Kg inclusive
+     // *  Se crea aquí el generador
+     // */
+    // public Paquete(double alto, double ancho, double largo)    {
+        // TODO
 
-    }
+    // }
 
-    /**
-     * Constructor 3 
-     * Crea el paquete a la dimension y peso indicados
-     * 
-     */
-    public Paquete(Dimension dimension, double peso)    {
-        //TODO
+    // /**
+     // * Constructor 3 
+     // * Crea el paquete a la dimension y peso indicados
+     // * 
+     // */
+    // public Paquete(Dimension dimension, double peso)    {
+        // TODO
 
-    }
+    // }
 
-    /**
-     * accesor para la dimensión
-     */
-    public Dimension getDimension() {
-        return this.dimension;
+    // /**
+     // * accesor para la dimensión
+     // */
+    // public Dimension getDimension() {
+        // return this.dimension;
 
-    }
+    // }
 
-    /**
-     * accesor para el peso
-     */
-    public double getPeso() {
-        return this.peso;
+    // /**
+     // * accesor para el peso
+     // */
+    // public double getPeso() {
+        // return this.peso;
 
-    }
+    // }
 
-    /**
-     * calcula y devuelve el volumen del paquete (en cm3)
-     * El volumen = alto x ancho x largo
-     */
-    public double calcularVolumen() {
-       //TODO
-       return 0;
+    // /**
+     // * calcula y devuelve el volumen del paquete (en cm3)
+     // * El volumen = alto x ancho x largo
+     // */
+    // public double calcularVolumen() {
+       // TODO
+       // return 0;
 
-    }
+    // }
 
-    /**
-     * calcula y devuelve el peso volumétrico del paquete (en Kg)
-     * Peso volumétrico = volumen del paquete / 5000
-     */
-    public double calcularPesoVolumetrico() {
-        //TODO
-       return 0;
+    // /**
+     // * calcula y devuelve el peso volumétrico del paquete (en Kg)
+     // * Peso volumétrico = volumen del paquete / 5000
+     // */
+    // public double calcularPesoVolumetrico() {
+        // TODO
+       // return 0;
 
-    }
+    // }
 
-    /**
-     *  Devuelve el peso facturable de un paquete
-     *  Siempre es el valor mayor entre su peso real y su peso volumétrico
-     *      
-     */
-    public double calcularPesoFacturable() {
-        //TODO
-       return 0;
+    // /**
+     // *  Devuelve el peso facturable de un paquete
+     // *  Siempre es el valor mayor entre su peso real y su peso volumétrico
+     // *      
+     // */
+    // public double calcularPesoFacturable() {
+        // TODO
+       // return 0;
 
-    }
+    // }
 
 
-    /**
-     * Devuelve una copia exacta al objeto actual
-     * Se obtienen copias también de los objetos que contenga
-     */
-    public Paquete obtenerCopia() {
-        //TODO
-       return null;
+    // /**
+     // * Devuelve una copia exacta al objeto actual
+     // * Se obtienen copias también de los objetos que contenga
+     // */
+    // public Paquete obtenerCopia() {
+        // TODO
+       // return null;
 
-    }
+    // }
 
-    /**
-     * Representación textual del paquete
-     *  (leer enunciado)
-     */
-    public String toString() {
-        //TODO
-       return null;
+    // /**
+     // * Representación textual del paquete
+     // *  (leer enunciado)
+     // */
+    // public String toString() {
+        // TODO
+       // return null;
 
-    }
+    // }
     
-    /**
-     * Muestra en pantalla el objeto actual
-     * Este método se incluye como método de prueba
-     * de la clase Paquete
-     */
-    public void print() {
-        System.out.println(this.toString());
-    }
+    // /**
+     // * Muestra en pantalla el objeto actual
+     // * Este método se incluye como método de prueba
+     // * de la clase Paquete
+     // */
+    // public void print() {
+        // System.out.println(this.toString());
+    // }
 
     
 
-}
+// }
